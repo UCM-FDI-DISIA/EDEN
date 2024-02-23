102ccbae8c1bc00b01d362cdbbb4ac993f6432e3
almasso <almasso@ucm.es>
2024-02-23 22:17:57 +0100

    MOD: Archivos de compilación automática arreglados

--- dependencies/Bullet/build.bat
+++ dependencies/Bullet/build.bat
@@ -2,22 +2,50 @@
 :: Especificamos las direcciones que vamos a usar
 :: COMPILEDIR es la dirección donde se encuentra el source de Bullet que vamos a compilar
 :: BUILDDIR es la dirección en la que vamos a volcar la build de Bullet
-set COMPILEDIR=..\src
+set COMPILEDIR=..\..\src
 set BUILDDIR=.\build
+set BULLETBUILDVER=1.0
+set COMPILE=1
+set PLATFORM=x64
 
-:: Creamos la carpeta en la ruta de BUILDDIR
-mkdir %BUILDDIR%
-cd %BUILDDIR%
+if exist chkbuild.EDENBUILD (
+    set /p CHKBUILDVER=<chkbuild.EDENBUILD
+    if "%CHKBUILDVER%"=="%BULLETBUILDVER%" (
+        set COMPILE=0
+    )
+) else (
+    echo %BULLETBUILDVER%>chkbuild.EDENBUILD
+)
 
-:: Generamos con CMake a partir de la carpeta con el Src de Bullet la solución de VSC++ con las tags correspondientes
-cmake %COMPILEDIR%
+if %COMPILE%==1 (
+    echo INICIANDO LA COMPILACIÓN DE BULLET
+    :: Creamos la carpeta en la ruta de BUILDDIR
+    mkdir %BUILDDIR%
+    cd %BUILDDIR%
 
-:: Compilamos Bullet tanto en Debug como en Release (solo hemos creado para x64, no tenemos que preocuparnos por Win32)
-msbuild "BULLET_PHYSICS.sln" /p:configuration=Debug /maxcpucount
-msbuild "BULLET_PHYSICS.sln" /p:configuration=Release /maxcpucount
+    if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+        mkdir x64
+        cd x64
+    ) else (
+        if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+            mkdir Win32
+            cd Win32
+            set PLATFORM=Win32
+        )
+    )
 
-:: En teoría Bullet no tiene .dll que mover a la carpeta donde se generan los .exe del motor
+    :: Generamos con CMake a partir de la carpeta con el Src de Bullet la solución de VSC++ con las tags correspondientes
+    cmake -A %PLATFORM% %COMPILEDIR%
 
-cd ..
+    :: Compilamos Bullet tanto en Debug como en Release (solo hemos creado para x64, no tenemos que preocuparnos por Win32)
+    msbuild "BULLET_PHYSICS.sln" /p:configuration=Debug /maxcpucount
+    msbuild "BULLET_PHYSICS.sln" /p:configuration=Release /maxcpucount
 
-echo Bullet compilado
+    :: En teoría Bullet no tiene .dll que mover a la carpeta donde se generan los .exe del motor
+
+    cd ..\..
+
+    echo Bullet compilado
+) else (
+    echo ULTIMA VERSION DE BULLET DETECTADA, no es necesario compilar
+)
--- dependencies/Ogre/build.bat
+++ dependencies/Ogre/build.bat
@@ -3,28 +3,56 @@
 :: COMPILEDIR es la dirección donde se encuentra el source de OGRE que vamos a compilar
 :: BUILDDIR es la dirección en la que vamos a volcar la build de OGRE
 :: DLLFOLDERS es la carpeta donde vamos a meter las DLL de OGRE una vez se compile todo
-set COMPILEDIR=..\src
+set COMPILEDIR=..\..\src
 set BUILDDIR=.\build
-set DLLFOLDERS=.\..\..\..\bin
+set DLLFOLDERS=.\..\..\..\..\bin
+set OGREBUILDVER=1.0
+set COMPILE=1
+set PLATFORM=x64
 
-:: Creamos la carpeta en la ruta de BUILDDIR
-mkdir %BUILDDIR%
-cd %BUILDDIR%
+if exist chkbuild.EDENBUILD (
+    set /p CHKBUILDVER=<chkbuild.EDENBUILD
+    if "%CHKBUILDVER%"=="%OGREBUILDVER%" (
+        set COMPILE=0
+    )
+) else (
+    echo %OGREBUILDVER%>chkbuild.EDENBUILD
+)
 
-:: Generamos con CMake a partir de la carpeta con el Src de OGRE la solución de VSC++ con las tags correspondientes
-cmake -DOGRE_BUILD_COMPONENT_BULLET:BOOL="0" -DOGRE_BUILD_COMPONENT_BITES:BOOL="0" %COMPILEDIR%
+if %COMPILE%==1 (
+    echo INICIANDO LA COMPILACIÓN DE OGRE
+    :: Creamos la carpeta en la ruta de BUILDDIR
+    mkdir %BUILDDIR%
+    cd %BUILDDIR%
 
-:: Compilamos OGRE tanto en Debug como en Release (solo hemos creado para x64, no tenemos que preocuparnos por Win32)
-msbuild "OGRE.sln" /p:configuration=Debug /maxcpucount
-msbuild "OGRE.sln" /p:configuration=Release /maxcpucount
+    if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+        mkdir x64
+        cd x64
+    ) else (
+        if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+            mkdir Win32
+            cd Win32
+            set PLATFORM=Win32
+        )
+    )
 
-:: Movemos las DLLs de OGRE generadas a la carpeta con ruta DLLFOLDERS
-:: /y suprime la solicitud para confirmar que desea sobrescribir un archivo de destino existente.
-:: /s copia directorios y subdirectorios, a menos que estén vacíos.
+    :: Generamos con CMake a partir de la carpeta con el Src de OGRE la solución de VSC++ con las tags correspondientes
+    cmake -A %PLATFORM% -DOGRE_BUILD_COMPONENT_BULLET:BOOL=0 -DOGRE_BUILD_COMPONENT_BITES:BOOL=0 %COMPILEDIR%
 
-XCOPY /y /s .\bin\debug\OgreMain_d.dll %DLLFOLDERS%
-XCOPY /y /s .\bin\release\OgreMain.dll %DLLFOLDERS%
+    :: Compilamos OGRE tanto en Debug como en Release (solo hemos creado para x64, no tenemos que preocuparnos por Win32)
+    msbuild "OGRE.sln" /p:configuration=Debug /maxcpucount
+    msbuild "OGRE.sln" /p:configuration=Release /maxcpucount
 
-cd ..
+    :: Movemos las DLLs de OGRE generadas a la carpeta con ruta DLLFOLDERS
+    :: /y suprime la solicitud para confirmar que desea sobrescribir un archivo de destino existente.
+    :: /s copia directorios y subdirectorios, a menos que estén vacíos.
 
-echo OGRE compilado
+    XCOPY /y /s .\bin\debug\OgreMain_d.dll %DLLFOLDERS%
+    XCOPY /y /s .\bin\release\OgreMain.dll %DLLFOLDERS%
+
+    cd ..\..
+
+    echo OGRE compilado
+) else (
+    echo ULTIMA VERSION DE OGRE DETECTADA, no es necesario compilar
+)

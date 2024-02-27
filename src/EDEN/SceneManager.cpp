e0999a167261bcbd09ccde91a86e5c49d0ec5ad9
Andrea Vega Saugar <anvega03@ucm.es>
2024-02-27 10:00:13 +0100

    SceneManager

---
+++ src/EDEN/SceneManager.cpp
@@ -0,0 +1,56 @@
+#include "SceneManager.h"
+#include <iostream>W
+#include "Scene.h";
+
+SceneManager::~SceneManager() {
+	for (auto it = _scenes.begin(); it != _scenes.end();) {
+		delete (*it);
+		it = _scenes.erase(it);
+	}
+	if (_activeScene != nullptr) delete _activeScene;
+}
+
+Scene* SceneManager::PushScene(const std::string& ID) {
+	Scene* newSc = new Scene(ID);
+	_scenes.push_front(newSc);
+	_activeScene = newSc;
+	return newSc;
+}
+
+Scene* SceneManager::ChangeScene(const std::string& ID) {
+	EmptyStack();
+	PushScene(ID);
+}
+
+void SceneManager::PopScene() {
+	_scenes.front()->SetToDestroy();
+	_scenes.pop_front();
+}
+
+void SceneManager::PopUntil(const std::string& ID) {
+	while (_scenes.front()->GetSceneID() != ID) PopScene();
+}
+
+void SceneManager::EmptyStack() {
+	while (_scenes.size() > 0) PopScene();
+}
+
+void SceneManager::Update(float dt) {
+	_scenes.front()->Update(dt);
+
+	auto it = _scenes.end();
+	it--;
+	for (; it >= _scenes.begin();) {
+		if((*it)->GetToRender()) (*it)->Render();
+		if (it > _scenes.begin()) --it;
+		else break;
+	}
+
+	for (auto it = _scenes.begin(); it != _scenes.end();) {
+		if ((*it)->GetToDestroy()) {
+			delete (*it);
+			it = _scenes.erase(it);
+		}
+		else ++it;
+	}
+}
---
+++ src/EDEN/SceneManager.h
@@ -0,0 +1,52 @@
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+
+#include <deque>
+#include <string>
+
+#include "Singleton.h"
+
+class Scene;
+class SceneManager : public Singleton<SceneManager> {
+	friend Singleton<SceneManager>;
+
+public:
+	SceneManager(){};
+	virtual ~SceneManager();
+
+	/// @brief Metodo encargado de crear una nueva escena y añadirla a la lista doblemente enlazada
+	/// @param ID El nombre que identifica a la escena
+	/// @return El puntero a la escena recién creada
+	Scene* PushScene(const std::string& ID);
+
+	/// @brief Metodo encargado de vaciar la lista de escenas y añadir una nueva
+	/// @param ID El nombre que identifica a la escena
+	/// @return El puntero a la escena recién creada
+	Scene* ChangeScene(const std::string& ID);
+
+	/// @brief Metodo encargado de eliminar la primera escena de la lista
+	void PopScene();
+
+	/// @brief Metodo encargado de eliminar la(s) primera(s) escena(s) de la lista hasta encontrar
+	/// una con el ID indicado
+	/// @param ID El nombre que identifica a la escena
+	void PopUntil(const std::string& ID);
+
+	/// @brief Metodo encargado de eliminar todas las escenas de la lista
+	void EmptyStack();
+
+	/// @brief Metodo que llama al Update de la primera escena de la lista, y al render de todas las
+	/// escenas que lo indiquen. Ademas, al final del bucle elimina las escenas que lo requieran;
+	/// @param dt El DeltaTime
+	void Update(float dt);
+
+	/// @brief Metodo que devuelve la escena la primera escena de la lista
+	/// @return La escena activa
+	inline Scene* GetCurrentScene() { return _activeScene; }
+
+private:
+	Scene* _activeScene = nullptr;
+	std::deque<Scene*> _scenes;
+};
+
+#endif // SCENE_MANAGER_H

6270c5ce9e3c7e217f0ed7b8ef8c16144dff9ce1
Paula López <paulal18@ucm.es>
2024-03-05 17:46:23 +0100

    [NO ACABADO] Wrapper MeshRenderer añadida al vcxproj

--- src/EDEN_Render/MeshRenderer.cpp
+++ src/EDEN_Render/MeshRenderer.cpp
@@ -2,18 +2,10 @@
 #include "Vector3.h"
 #include "Quaternion.h"
 
-render_wrapper::MeshRenderer::MeshRenderer()
+render_wrapper::MeshRenderer::MeshRenderer(const std::string entityID, const std::string meshName)
 {
 }
 
 render_wrapper::MeshRenderer::~MeshRenderer()
-{
-}
-
-void render_wrapper::MeshRenderer::setPositon(const eden_utils::Vector3 pos, const std::string id)
-{
-}
-
-void render_wrapper::MeshRenderer::setOrientation(const eden_utils::Quaternion qua, const std::string id)
 {
 }
--- src/EDEN_Render/MeshRenderer.h
+++ src/EDEN_Render/MeshRenderer.h
@@ -2,37 +2,29 @@
 #ifndef MESH_RENDERER_H
 #define MESH_RENDERER_H
 
-#include <string> // si??
+#include <string>
+#include "RenderObject.h"
 
 namespace eden_utils {
 	class Vector3;
 	class Quaternion;
 }
 
-namespace eden_render {
-	class RenderManager;
-}
 namespace render_wrapper {
 
 	class RenderObject;
 	class MeshRenderer : public render_wrapper::RenderObject
 	{
 	public:
-		MeshRenderer(/*id de la entidad*/ /*mesh*/); // _ent = new(), NodeManager():Instance->attach(_ent, id)
+		MeshRenderer(const std::string entityID, const std::string meshName); // _ent = new(), NodeManager():Instance->attach(_ent, id)
 		~MeshRenderer();
 
-		//void setPositon(const eden_utils::Vector3 pos, const std::string id);
-		//void setOrientation(const eden_utils::Quaternion qua, const std::string id);
-
-		void setMaterial();
+		void SetMaterial();
 		// getMaterial
 		// setInvisible
 
 	private:
-		//render_wrapper::Node* node_manager = nullptr;
-		// instancia de node manager 
-
-		// Ogre::Enitty* ent entidad de ogre (attach en la constructora)
+		Ogre::Entity* _ent = nullptr;
 		// string id 
 	};
 }

bbf17b2aaf972c7f299603b783d17273f45fe377
almasso <almasso@ucm.es>
2024-03-18 18:40:32 +0100

    Arreglada refactorizacion Lua

--- src/EDEN_Script/LuaManager.h
+++ src/EDEN_Script/LuaManager.h
@@ -44,9 +44,8 @@ namespace eden_script {
 
 		template <class T>
 		void Regist(T a) {
-
 			luabridge::getGlobalNamespace(L_)
-				.beginClass<a>("Hito1Prueba")
+				.beginClass<T>("Hito1Prueba")
 				.endClass();
 		}
 

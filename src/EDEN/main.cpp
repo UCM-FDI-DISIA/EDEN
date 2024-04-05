f478b65772bf34340fb76987e36603000818cea9
theligero <iligero@ucm.es>
2024-04-05 10:36:42 +0200

    main.cpp cambiado a versión main

--- src/EDEN/main.cpp
+++ src/EDEN/main.cpp
@@ -37,30 +37,56 @@
 
 int main() {
 
-#ifdef _DEBUG
-	HMODULE game = LoadLibraryA("game_d.dll");
-#else
-	HMODULE game = LoadLibraryA("game.dll");
-#endif
-	if (game == NULL) { // mejor con assert
-		std::cerr << "no se ha cargado la dll correctamente" << std::endl;
+	// Registramos el componente Transform, que es el unico que usaremos de momento
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CTransform>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_render::CCamera>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CImage>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito1Prueba>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAnimator>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CButton>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCursor>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CRigidBody>();
+
+
+	try
+	{
+		eden::Master* master = eden::Master::Instance();
+		//Creamos una escena inicial de pueba 
+		eden::SceneManager* scnManager = eden::SceneManager::Instance();
+		scnManager->PushScene("test_scene");
+		master->Loop();
+		delete scnManager;
+		delete master;
 	}
 
-	else {
-		typedef std::string (*SaludoFunc)();
-		// SaludoFunc saludo = (SaludoFunc)(GetProcAddress(game, "saludo"));
-		SaludoFunc saludo = reinterpret_cast<SaludoFunc>(GetProcAddress(game, "saludo"));
+	catch (std::exception e) {}
 
-		if (saludo == NULL) {
-			std::cerr << "no existe el metodo saludo de la dll" << std::endl;
-		}
-
-		else {
-			std::cout << saludo() << std::endl;
-		}
-
-		FreeLibrary(game);
-	}
+//#ifdef _DEBUG
+//	HMODULE game = LoadLibraryA("game_d.dll");
+//#else
+//	HMODULE game = LoadLibraryA("game.dll");
+//#endif
+//	if (game == NULL) { // mejor con assert
+//		std::cerr << "no se ha cargado la dll correctamente" << std::endl;
+//	}
+//
+//	else {
+//		typedef std::string (*SaludoFunc)();
+//		// SaludoFunc saludo = (SaludoFunc)(GetProcAddress(game, "saludo"));
+//		SaludoFunc saludo = reinterpret_cast<SaludoFunc>(GetProcAddress(game, "saludo"));
+//
+//		if (saludo == NULL) {
+//			std::cerr << "no existe el metodo saludo de la dll" << std::endl;
+//		}
+//
+//		else {
+//			std::cout << saludo() << std::endl;
+//
+//		}
+//
+//		FreeLibrary(game);
+//	}
 	
 	return 0;
 }

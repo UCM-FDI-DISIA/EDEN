03d9f337d2909eb4efae3f991899f4dd786d6894
DavidRainder <91317502+DavidRainder@users.noreply.github.com>
2024-04-08 21:40:05 +0200

    Faltaban Lua y Luabridge

--- src/EDEN_Main/main.cpp
+++ src/EDEN_Main/main.cpp
@@ -64,73 +64,72 @@
 #include <string>
 
 void RegisterComponents() {
-	// Registramos el componente Transform, que es el unico que usaremos de momento
 	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CTransform>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCamera>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CImage>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CText>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito1Prueba>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAnimator>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CButton>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CLuaBehaviour>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCursor>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CRigidBody>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CParticleEmitter>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioEmitter>();
-	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioListener>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCamera>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CImage>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CText>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito1Prueba>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAnimator>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CButton>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CLuaBehaviour>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCursor>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CRigidBody>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CParticleEmitter>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioEmitter>();
+	//eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioListener>();
 }
 
 int main(int argc, char* argv[]) {
 
-	//char *leak;
-	//leak = new char[256];
-	//strcpy_s(leak, 256, "LEAK CANARIO");
-
-	// Memory Leaks Check
-#ifdef _MSC_VER
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-
-	// Registro de componentes
-	RegisterComponents();
-
-	// Cogemos una instancia del manejador de errores
-	eden_error::ErrorHandler* errorHandler = eden_error::ErrorHandler::Instance();
-
-	// Hacemos try-catch de las excepciones generadas.
-	try
-	{
-#ifdef __APPLE__
-		std::filesystem::current_path(std::filesystem::canonical(std::string(argv[0]) + "/.."));
-#endif
-		eden::Master* master = eden::Master::Instance();
-		//Creamos una escena inicial de pueba 
-		eden::SceneManager* scnManager = eden::SceneManager::Instance();
-		/// ----------- TESTEO REFACTORIZACION DE ESCENAS, NO BORRAR --------------------
-		/*scnManager->PushScene("test_scene");
-		scnManager->PushScene("test_scene2");		
-		scnManager->PopScene();*/
-		/// ------------------------------------------------------------------------------ 
-		
-		scnManager->PushScene("Menu");
-		//scnManager->PushScene("CrossThePathFinal");
-		master->Loop();
-		master->Close();
-	}
-#ifdef __clang__
-	catch (std::runtime_error e) {
-		errorHandler->HandleExceptionNWin(e);
-	}
-#endif
-#ifdef _MSC_VER
-	catch (std::exception e){
-		// en caso de generar una excepci�n no tratada, se llamar� a este m�todo, que genera (en windows) un pop-up informando del error
-		errorHandler->HandleException(e);
-	}
-#endif
-	errorHandler->Close();
+//	//char *leak;
+//	//leak = new char[256];
+//	//strcpy_s(leak, 256, "LEAK CANARIO");
+//
+//	// Memory Leaks Check
+//#ifdef _MSC_VER
+//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//#endif
+//
+//	// Registro de componentes
+//	RegisterComponents();
+//
+//	// Cogemos una instancia del manejador de errores
+//	eden_error::ErrorHandler* errorHandler = eden_error::ErrorHandler::Instance();
+//
+//	// Hacemos try-catch de las excepciones generadas.
+//	try
+//	{
+//#ifdef __APPLE__
+//		std::filesystem::current_path(std::filesystem::canonical(std::string(argv[0]) + "/.."));
+//#endif
+//		eden::Master* master = eden::Master::Instance();
+//		//Creamos una escena inicial de pueba 
+//		eden::SceneManager* scnManager = eden::SceneManager::Instance();
+//		/// ----------- TESTEO REFACTORIZACION DE ESCENAS, NO BORRAR --------------------
+//		/*scnManager->PushScene("test_scene");
+//		scnManager->PushScene("test_scene2");		
+//		scnManager->PopScene();*/
+//		/// ------------------------------------------------------------------------------ 
+//		
+//		scnManager->PushScene("Menu");
+//		//scnManager->PushScene("CrossThePathFinal");
+//		master->Loop();
+//		master->Close();
+//	}
+//#ifdef __clang__
+//	catch (std::runtime_error e) {
+//		errorHandler->HandleExceptionNWin(e);
+//	}
+//#endif
+//#ifdef _MSC_VER
+//	catch (std::exception e){
+//		// en caso de generar una excepci�n no tratada, se llamar� a este m�todo, que genera (en windows) un pop-up informando del error
+//		errorHandler->HandleException(e);
+//	}
+//#endif
+//	errorHandler->Close();
 	return 0;
 }
 

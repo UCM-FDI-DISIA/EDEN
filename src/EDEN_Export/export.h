f6f5396585498cac6c0fd5494c1626cc120f58fe
theligero <iligero@ucm.es>
2024-04-11 19:10:15 +0200

    Revert "Pequeño ajuste"
    This reverts commit 7db556c45df747cd56ada186084e130427b3b0d4.

--- src/EDEN_Export/export.h
+++ src/EDEN_Export/export.h
@@ -19,8 +19,8 @@ namespace eden_export {
 	__declspec(dllexport) void StopEDEN();
 }
 
-eden::Master* master = nullptr;
-eden::SceneManager* scnManager = nullptr;
-eden_error::ErrorHandler* errorHandler = nullptr;
+eden::Master* master;
+eden::SceneManager* scnManager;
+eden_error::ErrorHandler* errorHandler;
 
 #endif /*EDEN_EXPORT_H_*/

0fa96ea10efbe41d5ea0de4eb78b791c2daca90e
theligero <iligero@ucm.es>
2024-04-11 17:00:54 +0200

    FUNCIONA
    me cago en mi vida

--- src/EDEN_Export/export.cpp
+++ src/EDEN_Export/export.cpp
@@ -1,80 +1,130 @@
+// Memory Leaks Check
 #define _CRTDBG_MAP_ALLOC
-#ifdef _DEBUG
-#include <iostream>
+#include <stdlib.h>
+#ifdef _MSC_VER
+#include <crtdbg.h>
 #endif
 
-#ifdef _MSC_VER
-#include <windows.h>
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new new(_NORMAL_BLOCK,__FILE__,__LINE__)
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
 #endif
-#include <ctime>
-#include <chrono>
+
+
+#include <iostream>
+#include <filesystem>
 
 #include "ErrorHandler.h"
 
-#include "export.h"
-#include <RenderManager.h>
-#include <PhysicsManager.h>
-#include <InputManager.h>
-#include <SceneManager.h>
-#include <Scene.h>
-#include <AudioManager.h>
+/// Engine Loop3
+#include "EdenMaster.h"
 
-eden_export::Master::Master() {
-	// la comprobación de que se haya podido inicializar el RenderManager ahora se hace dentro del propio RenderManager.
-	_renderManager = eden_render::RenderManager::Instance("EDEN Engine");
+/// Entity-Component
+#include "Entity.h"
+#include "Component.h"
+#include "ComponentFactory.h"
+#include "TestComponent.h"
+#include "Transform.h"
+#include "CMeshRenderer.h"
 
-	if (!_renderManager->couldInitialize()) delete _renderManager;
+/// Engine Render
+#include "CImage.h"
+#include "CText.h"
+#include "CButton.h"
+#include "CCursor.h"
+#include "CCamera.h"
+#include "CParticleEmitter.h"
+#include "CLight.h"
 
-	_inputManager = eden_input::InputManager::Instance();
-	_scnManager = eden::SceneManager::Instance();
-	_physicsManager = physics_manager::PhysicsManager::Instance();
+/// Engine Physics
+#include "CRigidBody.h"
+#include "CLuaBehaviour.h"
+
+// Engine Script
+#include <ScriptManager.h>
+#include "SceneManager.h"
+#include "Scene.h"
+
+// Engine Audio
+#include "CAudioEmitter.h"
+#include "CAudioListener.h"
+
+#include "Hito1Prueba.h"
+// #include "Hito2Prueba.h"
+// #include "Hito2MenuPausa.h"
+#include "CAnimator.h"
+
+#include "RenderManager.h"
+
+
+#include <string>
+#include "Export.h"
+
+void RegisterComponents() {
+	// Registramos el componente Transform, que es el unico que usaremos de momento
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CTransform>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCamera>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CImage>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CText>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CLight>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito1Prueba>();
+	// eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito2Prueba>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAnimator>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CButton>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CLuaBehaviour>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CCursor>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CRigidBody>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CMeshRenderer>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CParticleEmitter>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioEmitter>();
+	eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::CAudioListener>();
+	// eden_ec::ComponentFactory::Instance()->RegisterComponent<eden_ec::Hito2MenuPausa>();
 }
 
-void eden_export::Master::Loop() {
-	const std::chrono::steady_clock::time_point loopStartTime = std::chrono::high_resolution_clock::now();
+void eden_export::RunEDEN()
+{
+	// Memory Leaks Check
+	#ifdef _MSC_VER
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	#endif
 
-	std::chrono::steady_clock::time_point frameStartTime, frameEndTime;
+	// Registro de componentes
+	RegisterComponents();
 
-	float lastPhysicsUpdateTime = 0;
+	// Cogemos una instancia del manejador de errores
+	errorHandler = eden_error::ErrorHandler::Instance();
 
-	while (!exit) {
-		int numPU = (int)((_elapsedTime - lastPhysicsUpdateTime) / (_physicsUpdateTimeInterval));
-		if (numPU > 0) {
-			//std::string sceneID = _scnManager->GetCurrentScene()->GetSceneID();
-			//_physicsManager->UpdatePositions(sceneID);
-			//for (int i = 0; i < numPU; ++i) {
-			//	_physicsManager->updateSimulation(_physicsUpdateTimeInterval, sceneID);
-			//}
-			//_physicsManager->ResolvePositions(sceneID);
-		}
-		lastPhysicsUpdateTime = lastPhysicsUpdateTime + (numPU * _physicsUpdateTimeInterval);
+	// Hacemos try-catch de las excepciones generadas.
+	try
+	{
+#ifdef __APPLE__
+		std::filesystem::current_path(std::filesystem::canonical(std::string(argv[0]) + "/.."));
+#endif
+		master = eden::Master::Instance();
+		//Creamos una escena inicial de pueba 
+		scnManager = eden::SceneManager::Instance();
+		/// ----------- TESTEO REFACTORIZACION DE ESCENAS, NO BORRAR --------------------
+		//scnManager->PushScene("test_scene");
+		/*scnManager->PushScene("test_scene2");
+		scnManager->PopScene();*/
+		/// ------------------------------------------------------------------------------ 
 
-		frameStartTime = std::chrono::high_resolution_clock::now();
+		scnManager->PushScene("Menu");
+		master->Loop();
+	}
 
-		_inputManager->Update();
-		_scnManager->Update(_deltaTime);
-		_renderManager->UpdatePositions(_scnManager->GetCurrentScene()->GetSceneID());
-		_renderManager->Update();
-		exit = _inputManager->CloseWindowEvent();
-		if (_inputManager->ResizedWindowEvent()) {
-			_renderManager->ResizedWindow();
-		}
-
-		frameEndTime = std::chrono::high_resolution_clock::now();
-		_deltaTime = std::chrono::duration<float, std::milli>(frameEndTime - frameStartTime).count() / 1000;
-		_elapsedTime = std::chrono::duration<float, std::milli>(frameEndTime - loopStartTime).count() / 1000;
+	catch (std::exception e) {
+		errorHandler->HandleException(e);
 	}
 }
 
-eden_export::Master::~Master() {
-	_physicsManager->Close();
-	_scnManager->Close();
-	_inputManager->Close();
-	eden_audio::AudioManager::Instance()->Close();
-
-	if (_renderManager != nullptr && _renderManager->couldInitialize()) {
-
-		//delete _renderManager;
-		_renderManager->Close();
-	}
+void eden_export::StopEDEN()
+{
+	master->Close();
+	errorHandler->Close();
 }
--- src/EDEN_Export/export.h
+++ src/EDEN_Export/export.h
@@ -1,74 +1,26 @@
 #ifndef EDEN_EXPORT_H_
 #define EDEN_EXPORT_H_
 
-#include "Singleton.h"
-
-namespace Ogre {
-	class Root;
-}
-namespace eden_render {
-	class RenderManager;
-}
-namespace eden_input {
-	class InputManager;
-}
-
-namespace physics_manager {
-	class PhysicsManager;
-}
-
 namespace eden {
+	class Master;
 	class SceneManager;
+
+	Master* master;
+	SceneManager* scnManager;
 }
 
-namespace eden_export
-{
-	/// @brief El master es la clase singleton encargada de llevar el bucle principal de juego
-	/// @brief Es esta clase que gestiona el tiempo del juego
-	class __declspec(dllexport) Master : public Singleton<Master>
-	{
-		friend Singleton<Master>;
-
-	public:
-		static bool isInitialized() { return true; }
-
-		/// @brief Booleano de salida del bucle principal
-		bool exit = false;
-
-		/// @brief Bucle principal de juego
-		void Loop();
-
-		void CloseApplication();
-
-		/// @brief Destructora por defecto de EdenMaster
-		~Master() override;
-	private:
-
-		// static bool _initialized;
-		/// @brief El tiempo entre frames en segundos
-		float _deltaTime = 0;
-
-		/// @brief El intervalo de tiempo (en segundos) que pasa entre dos actualizaciones físicas
-		const float _physicsUpdateTimeInterval = 1.0f / 60.0f;
-
-		/// @brief El tiempo transcurrido desde el inicio del bucle en segundos
-		float _elapsedTime = 0;
-
-		/// @brief Referencia a al InputManager
-		eden_input::InputManager* _inputManager;
-
-		/// @brief Referencia a al SceneManager
-		eden::SceneManager* _scnManager;
-
-		/// @brief Referencia a al RenderManager
-		eden_render::RenderManager* _renderManager;
-
-		/// @brief Referencia al PhysicsManager
-		physics_manager::PhysicsManager* _physicsManager;
-
-		/// @brief Constructora por defecto de EdenMaster
-		Master();
-	};
+namespace eden_error {
+	class ErrorHandler;
+	ErrorHandler* errorHandler;
 }
+
+namespace eden_export {
+	__declspec(dllexport) void RunEDEN();
+	__declspec(dllexport) void StopEDEN();
+}
+
+eden::Master* master;
+eden::SceneManager* scnManager;
+eden_error::ErrorHandler* errorHandler;
 
 #endif /*EDEN_EXPORT_H_*/

5e59c6d3cfeaab1375c75747c458c874ff2dc79b
almasso <almasso@ucm.es>
2024-04-15 23:26:33 +0200

    Otra actualización de los bat para la compilación de las librerías

--- dependencies/Lua/build.bat
+++ dependencies/Lua/build.bat
@@ -40,8 +40,8 @@ if !COMPILE! equ 1 (
     :: Generamos con CMake a partir de la carpeta con el Src de Lua la solución de VSC++ con las tags correspondientes
     cmake -A %PLATFORM% %COMPILEDIR%
     :: Compilamos Lua tanto en Debug como en Release (solo hemos creado para x64, no tenemos que preocuparnos por Win32)
-    msbuild "LUA_C.sln" /p:configuration=Debug /maxcpucount
-    msbuild "LUA_C.sln" /p:configuration=Release /maxcpucount
+    msbuild "LUA_C.sln" /p:configuration=Debug /p:Platform=x64 /maxcpucount
+    msbuild "LUA_C.sln" /p:configuration=Release /p:Platform=x64 /maxcpucount
     :: En teoría Bullet no tiene .dll que mover a la carpeta donde se generan los .exe del motor
     cd ..\..
     echo Lua compilado

cc44c45d8f341013f8ba9e06484416f5efa97cb4
DavidRainder <davirive@ucm.es>
2024-04-24 11:59:20 +0200

    Modificar RigidBody para evitar desactivación temporal

--- src/EDEN_Physics/RigidBody.cpp
+++ src/EDEN_Physics/RigidBody.cpp
@@ -77,6 +77,8 @@ physics_wrapper::RigidBody::RigidBody(eden_ec::Entity* ent, const ShapeParameter
 	SetBounciness(bounciness);
 
 	_collisionCallback = new CollisionCallback(this);
+
+	_rigidBody->setActivationState(DISABLE_DEACTIVATION);
 }
 
 physics_wrapper::RigidBody::~RigidBody()

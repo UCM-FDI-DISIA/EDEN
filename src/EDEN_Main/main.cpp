85aa9ee2e456f019e2cb85133d2773d522e47dc9
DavidRainder <davirive@ucm.es>
2024-05-10 04:39:25 +0200

    Sin consola en Release

--- src/EDEN_Main/main.cpp
+++ src/EDEN_Main/main.cpp
@@ -1,7 +1,13 @@
-#include "Export.h"
 #include <iostream>
+#include <windows.h>
 
+#include "Export.h"
+
+#ifdef _DEBUG
 int main(int argc, char* argv[]) {
+#else 
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCadShow) {
+#endif
 	// Memory Leaks Check
 #ifdef _MSC_VER
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);

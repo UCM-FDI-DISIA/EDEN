f685f56d70d91a8bdde5352fb6db8a1ae208ede2
theligero <iligero@ucm.es>
2024-06-30 19:53:44 +0200

    quitada la build de zlib
    fallo mío

--- dependencies/zlib/build/x64/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ilige/OneDrive/Documentos/GDV/Tercero/P3/CrossthePath/EDEN/dependencies/zlib/src
-# Build directory: C:/Users/ilige/OneDrive/Documentos/GDV/Tercero/P3/CrossthePath/EDEN/dependencies/zlib/build/x64
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
